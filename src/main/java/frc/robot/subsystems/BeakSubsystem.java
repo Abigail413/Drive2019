4b7380d4d56cbf5264a6f538c7ac5107aaea525c
Abigail413 <56323310+Abigail413@users.noreply.github.com>
2020-01-30 18:23:44 -0600

    Add files via upload

--- src/main/java/frc/robot/subsystems/BeakSubsystem.java
+++ src/main/java/frc/robot/subsystems/BeakSubsystem.java
@@ -7,36 +7,47 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 /**
  * Add your docs here.
  */
-public class DriveSubsystem extends SubsystemBase{
+public class BeakSubsystem extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  WPI_TalonSRX RightFrontWheel = new WPI_TalonSRX(2);
-  WPI_TalonSRX LeftFrontWheel = new WPI_TalonSRX(4);
-  WPI_TalonSRX RightRearWheel = new WPI_TalonSRX(1);
-  WPI_TalonSRX LeftRearWheel = new WPI_TalonSRX(3);
-  
-  DifferentialDrive roboDrive = new DifferentialDrive(LeftFrontWheel, RightFrontWheel);
+  Compressor Airow = new Compressor(21);
 
-  public DriveSubsystem() {
-    RightRearWheel.follow(RightFrontWheel);
-    LeftRearWheel.follow(LeftFrontWheel);
+  Solenoid BeakPusher = new Solenoid(21 , 2);
+
+  boolean beakOut = false;
+
+  public BeakSubsystem() {
+
   }
 
-  public void TankDrive(double leftControl, double rightControl) {
-    LeftFrontWheel.set(leftControl);
-    RightFrontWheel.set(rightControl);
+  public void out() {
+   BeakPusher.set(true);
+   beakOut = true;
+  }
+
+  public void in() {
+   BeakPusher.set(false);
+   beakOut = false;
   }
   
-  public DifferentialDrive getDifferentialDrive() {
-    return roboDrive;
+  public void switchState() {
+   if (beakOut = false) {
+    this.in();
+   }
+   else {
+    this.out();
+   }
+
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
--- src/main/java/frc/robot/subsystems/TailSubsystem.java
+++ src/main/java/frc/robot/subsystems/TailSubsystem.java
@@ -7,36 +7,47 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 /**
  * Add your docs here.
  */
-public class DriveSubsystem extends SubsystemBase{
+public class TailSubsystem extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  WPI_TalonSRX RightFrontWheel = new WPI_TalonSRX(2);
-  WPI_TalonSRX LeftFrontWheel = new WPI_TalonSRX(4);
-  WPI_TalonSRX RightRearWheel = new WPI_TalonSRX(1);
-  WPI_TalonSRX LeftRearWheel = new WPI_TalonSRX(3);
-  
-  DifferentialDrive roboDrive = new DifferentialDrive(LeftFrontWheel, RightFrontWheel);
+  Compressor Airow = new Compressor(21);
 
-  public DriveSubsystem() {
-    RightRearWheel.follow(RightFrontWheel);
-    LeftRearWheel.follow(LeftFrontWheel);
+  Solenoid TailPusher = new Solenoid(21 , 1);
+
+  boolean tailOut = false;
+
+  public TailSubsystem() {
+
   }
 
-  public void TankDrive(double leftControl, double rightControl) {
-    LeftFrontWheel.set(leftControl);
-    RightFrontWheel.set(rightControl);
+  public void out() {
+   TailPusher.set(true);
+   tailOut = true;
+  }
+
+  public void in() {
+   TailPusher.set(false);
+   tailOut = false;
   }
   
-  public DifferentialDrive getDifferentialDrive() {
-    return roboDrive;
+  public void switchState() {
+   if (tailOut = false) {
+    this.in();
+   }
+   else {
+    this.out();
+   }
+
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
